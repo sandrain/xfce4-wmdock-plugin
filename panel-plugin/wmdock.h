c447139af848f5868e1711549bd8bee5bd8c6b41
Andre Ellguth <andre@ellguth.com>
2007-08-29 18:39:12 +0000

    Initial revision
    (Old svn revision: 3091)

---
+++ panel-plugin/wmdock.h
@@ -0,0 +1,33 @@
+
+
+#ifndef __WMDOCK_H__
+#define __WMDOCK_H__
+
+#include <gtk/gtk.h>
+#include <libxfce4panel/xfce-panel-plugin.h>
+
+#define WNCK_I_KNOW_THIS_IS_UNSTABLE
+#include <libwnck/libwnck.h>
+
+typedef struct _dockapp {
+	WnckWindow *w;
+	GtkSocket *s;
+	GtkWidget *frame;
+	GdkNativeWindow i;
+	gchar *name;
+	gchar *cmd;
+} DockappNode;
+
+typedef struct {
+	XfcePanelPlugin *plugin;
+
+	GtkWidget *ebox;
+	
+	/* Plugin specific definitions */
+	GtkWidget *align;
+	GtkWidget *box;
+
+	GSList *dapps;
+} WmdockPlugin;
+
+#endif /* __WMDOCK_H__ */
